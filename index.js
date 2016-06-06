96b2dfe26b170aacef97ba0d9c4d6a852de355bd
Lukas Welinder <Lukas@Welinder.Co>
2016-06-06 15:40:09 -0700

    (feature) creates file-tree structure from object
    * test file is currently working
    * TODO
       .* jasmine testing with gulp
       .* test readstream pipe to writestream
       .* document functions in detail

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
+const config = require('./dirptree.config');
 
 const root_dir = path.join(__dirname, '../../');
 
 const dirp_file = (file, dir_path) => {
+  let file_path = path.join(dir_path,file.name||file);
+  let write_stream = fs.createWriteStream(file_path, {flags: 'wx'});
+  if(file.src)
+    fs.createReadStream(path.join(root_dir,file.src)).pipe(write_stream);
+  return write_stream;
 };
 
 const dirp_tree = function(dir_tree,root_path) {
+  dir_tree = dir_tree || config;
+  root_path = root_path || root_dir;
+  let dir_path = path.join(root_path,dir_tree.name);
+  return mkdirp(dir_path, (err, path) => {
+    if(err)
+      return err;
+    if(dir_tree.paths)
+      dir_tree.paths.forEach((path) => dirp_tree(dir,dir_path));
+    if(dir_tree.files)
+      dir_tree.files.forEach((file) => dirp_file(file,dir_path));
+  });
 };
 
 module.exports = dirp_tree;
