4e07e614e7189619663b644fd689c74a954152a0
Lukas Welinder <Lukas@Welinder.Co>
2016-06-06 15:30:34 -0700

    initial commit
    * basic skeleton of ‘drip-tree’ module
    * added .gitignore
    * dependencies installed / required:
       .* ‘fs’
       .* ‘path’
       .* ‘mkdirp’

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+
+const root_dir = path.join(__dirname, '../../');
+
+const dirp_file = (file, dir_path) => {
+};
+
+const dirp_tree = function(dir_tree,root_path) {
+};
+
+module.exports = dirp_tree;
