043d52615c410e9655a8394a1a17d219f5d2e871
Lukas Welinder <Lukas@Welinder.Co>
2016-06-06 19:44:18 -0700

    (chore) added detailed commentary
    * minor changes to the namespace of dirp_tree()
    * fixed issue with npm test command

--- index.js
+++ index.js
@@ -3,8 +3,33 @@ const path = require('path');
 const mkdirp = require('mkdirp');
 const config = require('./dirptree.config');
 
+/**
+ * root_dir - absolute path to location of
+ * node_modules folder (ie project root folder)
+ * 
+ * @type {[string]}
+ */
 const root_dir = path.join(__dirname, '../../');
 
+/*
+ * dirp_file - internal API used to create file and/or pipe
+ * to file from src defined in directory tree object; accepts
+ * either an object or a string for the argument 'file' and 
+ * behaves accordingly
+ * 
+ * @param  {[object || string]} file       file to be created as a writestream,
+ *                                         if it is an object with an 'src' property
+ *                                         then it is piped to from that file; src is
+ *                                         a relative path from the location of the
+ *                                         node_modules folder
+ * 
+ * @param  {[string]} dir_path            directory path from root into which the
+ *                                        file will be created
+ *                                        
+ * @return {[stream]} write_stream        returns instance of the writeStream class
+ *                                        currently unused, future implementation of
+ *                                        promises and/or vinyl streams for gulp planned
+ */
 const dirp_file = (file, dir_path) => {
   let file_path = path.join(dir_path,file.name||file);
   let write_stream = fs.createWriteStream(file_path, {flags: 'wx'});
@@ -13,6 +38,21 @@ const dirp_file = (file, dir_path) => {
   return write_stream;
 };
 
+/*
+ *  dirp_tree - filetree generator directly exported from module;
+ *  currently the return value has no funcitonality, implementation
+ *  of promises planned
+ * 
+ * @param  {[object]} dir_tree   object from which the directory tree will
+ *                               be constructed, formatting documentation
+ *                               can be found in dirptree.config.js,
+ *                               defaults to the obj in the config file
+ *                               
+ * @param  {[string]} root_path  absolute path of location to place dir_tree,
+ *                               defaults to location of node_modules folder
+ *
+ * @return {[stream]} mkdirp     currently does nothing as far as functionality
+ */
 const dirp_tree = function(dir_tree,root_path) {
   dir_tree = dir_tree || config;
   root_path = root_path || root_dir;
@@ -21,7 +61,7 @@ const dirp_tree = function(dir_tree,root_path) {
     if(err)
       return err;
     if(dir_tree.paths)
-      dir_tree.paths.forEach((path) => dirp_tree(dir,dir_path));
+      dir_tree.paths.forEach((dir) => dirp_tree(dir,dir_path));
     if(dir_tree.files)
       dir_tree.files.forEach((file) => dirp_file(file,dir_path));
   });
