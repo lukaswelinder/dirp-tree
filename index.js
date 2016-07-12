0efe350407cc480f5ed8e5e98fd2446770a20d13
Lukas Welinder <Lukas@Welinder.Co>
2016-07-11 17:08:23 -0700

    [chore/doc] code cleanup and descriptive comments

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict';
-
+// Node.js Core
 const fs = require('fs');
 const path = require('path');
-const stream = require('stream');
-
+// Non-Core Dependencies
 const co = require('co');
 const mkdirp = require('mkdirp');
-
 
 /*
  * ROOT_DIR - absolute path to location of
@@ -20,19 +18,33 @@ const ROOT_DIR = path.join(__dirname, '../../');
  * CONFIG - IIFE checks if file 'dirptree.CONFIG.js' exists
  * in project root-directory, if it does not, loads default from
  * package
+ * 
+ * @param  {[string]} file_path         TBD -- see TODO below
  *
- * @return {[stream]} write_stream
+ * @return {[module]} CONFIG            returns loaded configuration file in project-
+ *                                      root directory OR module default
  */
-const CONFIG = (function loadConfig(fp) {
-  let cfi_path = path.join(ROOT_DIR, fp || 'dirptree.config.js');
+const CONFIG = (function loadConfig(file_path) {
+  let cfi_path = path.join(ROOT_DIR, file_path || 'dirptree.config.js');
   try {
     return require(cfi_path);
   } catch(err) {
-    return require('./dirptree.CONFIG');
+    return require('./dirptree.config.js');
   }
 })();
 // TODO: apply 'loadConfig()' as prototype of the export to allow custom CONFIG locations
 
+/*
+ * mkdirp_p - promisified mkdirp module, recursively runs mkdir 
+ * command to create multiple directories from strings like 'foo/bar'
+ * 
+ * @param  {[string]} dir_path          directory path to create directory(ies) from;
+ *                                      the string 'foo/bar' would result in creating
+ *                                      directory 'foo' in PWD and 'bar' inside of 'foo'
+ *
+ * @return {[promise]} err/path         returns promise that resolves with the root-path
+ *                                      that was created or rejects with an error
+ */
 const mkdirp_p = (dir_path) =>
   new Promise(function(resolve, reject) {
     mkdirp(dir_path, (err, path) => {
@@ -49,9 +61,9 @@ const mkdirp_p = (dir_path) =>
  * either an object or a string for the argument 'file' and 
  * behaves accordingly
  * 
- * @param  {[object || string]} file       file to be created as a writestream,
- *                                         'src' property defines a file to pipe from
- *                                         relative to the project root (loc of node_modules)
+ * @param  {[object || string]} file      file to be created as a writestream,
+ *                                        'src' property defines a file to pipe from
+ *                                        relative to the project root (loc of node_modules)
  * 
  * @param  {[string]} dir_path            directory path from root into which the
  *                                        file will be created
@@ -73,8 +85,21 @@ const dirp_file = (file, dir_path) =>
     }
   });
 
-
-const dirp_dir_rec = (dir_tree, root_path) => {
+/*
+ *  dirp_dir_rec - recursively creates directory-tree
+ *  breadth-first, then creates/pipes to files depth first;
+ *  returns an array of streams ordered depth first to comply
+ *  with TDD of dependencies when used for concatenation/bundling
+ * 
+ * @param  {[object]} dir_tree            object from which the directory tree will
+ *                                        be constructed defaults to CONFIG(see comment)
+ *                               
+ * @param  {[string]} root_path           absolute path of location to place dir_tree,
+ *                                        defaults to location of node_modules folder
+ *
+ * @return {[array-of-stream]}            currently does nothing as far as functionality
+ */
+const dirp_dir_rec = function(dir_tree, root_path) {
   let dir_path = path.join(root_path, dir_tree.name || dir_tree);
   let p = mkdirp_p(dir_path);
   let rec, ret;
@@ -91,26 +116,18 @@ const dirp_dir_rec = (dir_tree, root_path) => {
 };
 
 /*
- *  dirp_tree - filetree generator directly exported from module;
- *  currently the return value has no functionality, implementation
- *  of promises or streams planned
+ *  dirp_tree - filetree generator directly exported from module
  * 
- * @param  {[object]} dir_tree   object from which the directory tree will
- *                               be constructed, formatting documentation
- *                               can be found in dirptree.CONFIG.js,
- *                               defaults to the obj in the CONFIG file
+ * @param  {[object]} dir_tree            object from which the directory tree will
+ *                                        be constructed defaults to CONFIG(see comment)
  *                               
- * @param  {[string]} root_path  absolute path of location to place dir_tree,
- *                               defaults to location of node_modules folder
+ * @param  {[string]} root_path           absolute path of location to place dir_tree,
+ *                                        defaults to location of node_modules folder
  *
- * @return {[stream]} mkdirp     currently does nothing as far as functionality
+ * @return {[stream]} dirp_dir_rec        currently does nothing as far as functionality
  */
-const dirp_tree = function(dir_tree, root_path) {
+module.exports = function(dir_tree, root_path) {
   dir_tree = dir_tree || CONFIG;
   root_path = root_path || ROOT_DIR;
-  return co(function*(){
-    return yield dirp_dir_rec(dir_tree, root_path);
-  });
+  return dirp_dir_rec(dir_tree, root_path);
 };
-
-module.exports = dirp_tree;
